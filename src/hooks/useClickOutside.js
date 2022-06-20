fbf5d9918a75ddc9092cc4e0968297b39ce1bd60
Bardugo <amirba@monday.com>
2022-06-20 17:05:49 +0300

    fixes

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -1,5 +1,5 @@
 export const useClickOutside = (ref, callback) => {
-  // TODO 1: this hook should get an element and a callback and will call it whenever you click outside of the element
+  // TODO 1: this hook should get a reference to an element and a callback, and will call it whenever you click outside of the element
 };
 
 const isClickOutside = (element, clickEvent) => {
