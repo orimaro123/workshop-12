996a8746ddec7185d64830f17e6b51a0576bab69
Bardugo <amirba@monday.com>
2022-06-21 09:29:28 +0300

    fix comments

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,4 +1,4 @@
 export const useDebounce = (value, delay) => {
-  // this hook should get a value and delay and return a debounced value
+  // TODO 3: this hook should get a value and delay and return a debounced value
   return value;
 };
--- src/hooks/useIsMounted.js
+++ src/hooks/useIsMounted.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 
 export const useIsMounted = () => {
-  // TODO 2: currently you are in a risk for an exception, if you'll start a search and close the modal the app will crash
+  // TODO 2: currently you are in a risk for an exception, if you'll start a search and close the modal the app will throw an error
   //         this is because you update the state of unmounted component, implement this hook to avoid this.
   //         this should return a function that would inidicate if the component is mounted or not
   return useCallback(() => true, []);
